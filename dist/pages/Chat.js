993af6ea1827cb99c4a0f6fe0539c16e1b1246b2
ffouquet42 <ffouquet42@gmail.com>
2025-05-10 16:26:03 +0200

    backroll before routes

--- dist/pages/Chat.js
+++
@@ -1,9 +0,0 @@
-// src/pages/Chat.ts
-export function renderChat() {
-    return `
-	  <section class="mt-12">
-		<h2 class="text-2xl font-semibold">Live Chat</h2>
-		<p class="text-gray-500 mt-2">[Chat interface here]</p>
-	  </section>
-	`;
-}
--- dist/pages/Home.js
+++
@@ -1,9 +0,0 @@
-// src/pages/Home.ts
-export function renderHome() {
-    return `
-	  <section class="text-center mt-12">
-		<h1 class="text-4xl font-bold text-indigo-700">Welcome to ft_transcendence</h1>
-		<p class="mt-4 text-gray-600">Play Pong, chat with friends, join tournaments!</p>
-	  </section>
-	`;
-}
